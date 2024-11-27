cf0f704c81df7b8769ea2a2da87afad22ec5fb81
sachin7con <sachin7con@gmail.com>
2024-11-27 13:02:09 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
+//SGN
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+const projectList = [
+      {name: "Smart-Way Coaching Institute app", 
+        description: "It should be helpful to manage students, fee, syllabus etc. "
+      },
+      { name: "myPofile app", 
+        description: "highlight my profile "
+      }, 
+
+      {
+        name: "ToDoApp",
+        description:" it should help to manage and remind daily task, weekly, monthly goals etc. ",
+      },
+]   
+
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+       {/* ONS  */}
+        {projectList.map((project, index) => (
+          <div key={index} className="project">
+            <h1 data-ns-test="project-name">{project.name}</h1>
+            <h6 data-ns-test="project-description">{project.description}</h6>
+          </div>
+        ))}
+
+    </div>
   )
 }
 
